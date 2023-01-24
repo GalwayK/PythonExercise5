e6ad1b53a4c9ba817c50820d5a4bb670f48a65d0
Kyle Galway <galwayk@sheridancollege.ca>
2023-01-23 21:27:49 -0500

    Add five nominal and functional improvements.
    Add links to email, specify language, limit news to predfined number, parameterize news category.

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ import send_email
 import datetime
 import unidecode
 
-URL = "https://newsapi.org/v2/everything?q=tesla&from=2022-12-24&sortBy=publishedAt&apiKey=d81785cd88a748cab6f18bf24d97" \
-      "f219"
+topic = "tesla"
+
+URL = "https://newsapi.org/v2/everything?" \
+      f"q={topic}" \
+      "&from=2022-12-24" \
+      "&sortBy=publishedAt&" \
+      "apiKey=d81785cd88a748cab6f18bf24d97f219" \
+      "&language=en"
+
 API_KEY = "d81785cd88a748cab6f18bf24d97f219"
 CURRENT_TIME = datetime.datetime.now().strftime("%A %B %Y")
 SUBJECT = f"News for {CURRENT_TIME}"
@@ -13,8 +20,8 @@ request = requests.get(url=URL)
 content = request.json()
 
 message = ""
-for article in content["articles"]:
-    message = f"{message}\n{article['title']}\n{article['description']}\n"
+for article in content["articles"][:20]:
+    message = f"{message}\n{article['title']}\n{article['description']}\n{article['url']}\n"
 
 message = unidecode.unidecode(message)
 send_email.email_news(message, SUBJECT)
